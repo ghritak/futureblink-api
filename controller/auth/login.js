b8ecfabb0b83c62f2195d5823aeb19a086aa1521
ghritak <ghritakjyotikalita@gmail.com>
2024-05-08 18:16:03 +0530

    login

--- controller/auth/login.js
+++ controller/auth/login.js
@@ -1,14 +1,38 @@
 const users = require('../../model/users');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await users.findOne({ email: email });
-  if (!user) return res.status(404).json({ error: 'User not found' });
+  try {
+    const { email, password } = req.body;
+    const user = await users.findOne({ email: email });
+    if (!user)
+      return res
+        .status(404)
+        .json({ error: 'User not found with the given email.' });
 
-  const isValidPassword = await bcrypt.compare(password, user.password);
-  if (!isValidPassword)
-    return res.status(400).json({ error: 'Invalid password.' });
-  return res.status(200).json({ message: 'Succesfully logged in.' });
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword)
+      return res.status(400).json({ error: 'Invalid password.' });
+
+    const authToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+    const userData = {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    };
+
+    return res.status(200).json({
+      message: 'Succesfully logged in.',
+      token: authToken,
+      data: userData,
+    });
+  } catch {
+    (err) => {
+      console.log(err);
+      return res.status(500).json({ error: 'Internal server error' });
+    };
+  }
 };
 
 module.exports = { login };
